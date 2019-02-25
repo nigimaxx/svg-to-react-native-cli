23b9d9569ba004b438d1227ba78667d26cae4b82
Matthias Martin <github@stichwort-m.de>
2019-02-25 18:05:14 +0100

    color default

--- src/replaceFill.js
+++ src/replaceFill.js
@@ -1,3 +1,5 @@
 module.exports = function replaceAll(str) {
-  return str.replace(/fill=".*?"/gm, 'fill={props.fill}');
+  return str.replace(/fill=(".*?")/gm, function (a, color) {
+    return 'fill={props.fill || ' + color + '}';
+  });
 };
